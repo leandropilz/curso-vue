e8fff3e33a2e1ad829b7ccaa0416b64a6d526d8f
pilz <leandro.pizz@gmail.com>
2019-05-08 15:50:49 -0300

    Desafio 01 concluido - CAP 19.

---
+++ dom/desafio-01/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Leandro Pilz',
+        idade: 38,
+        imagem: 'https://hips.hearstapps.com/amv-prod-cad-assets.s3.amazonaws.com/media/assets/submodel/8719.jpg'
+    }
+})
