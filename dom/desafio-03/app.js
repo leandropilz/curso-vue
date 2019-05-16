19de3124fe53ad6d2b8ea91ea56e55a301dd35ad
pilz <leandro.pizz@gmail.com>
2019-05-16 15:14:54 -0300

    CAP. 40 - Concluido o DOM.

--- dom/desafio-03/app.js
+++ dom/desafio-03/app.js
@@ -2,5 +2,21 @@ new Vue({
     el: '#desafio',
     data: {
         valor: 0
-    }
+    },
+    computed: {
+        /* utiliar propriedade computada(função), garante que só será chamada a func pelo contexto correto.
+        botão aumentar 2 não impacta no calculo do resultado, mas se olhar log, renderiza a chamada 
+        da func 
+        */
+        resultado() {
+            return this.valor == 37 ? 'Valor Igual' : 'Valor Diferente'
+        }
+    },
+    watch: {
+        resultado() {
+            setTimeout(() => {
+                this.valor = 0
+            }, 5000)
+        }
+    },
 });
