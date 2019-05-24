a08f0136d4bb126a88a2dd75acbceaa282a05dba
pilz <leandro.pizz@gmail.com>
2019-05-24 17:27:13 -0300

    CAP.108 Hora de Praticar: Organizar Projeto em Componentes(Resposta desafio).

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <app-menu/>
+    <v-toolbar app>
+      <app-toolbar/>
+    </v-toolbar>
+    <v-content>
+      <app-carousel/>
+    </v-content>
+    <app-footer/>
+  </v-app>
+</template>
+
+<script>
+import Menu from "./Menu.vue";
+import Toolbar from "./Toolbar.vue";
+import Carousel from "./Carousel.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: {
+    "app-menu": Menu,
+    "app-toolbar": Toolbar,
+    "app-carousel": Carousel,
+    "app-footer": Footer
+  }
+};
+</script>
