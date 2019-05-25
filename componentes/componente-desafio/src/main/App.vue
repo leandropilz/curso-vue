238526fb57c2a38a42bc5e1c2171d26865098c7c
pilz <leandro.pizz@gmail.com>
2019-05-25 00:58:05 -0300

    CAP.112 Conclusao do modulo incial de componentes.

---
+++ componentes/componente-desafio/src/main/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app>
+    <Menu/>
+    <v-toolbar app>
+      <Toolbar/>
+    </v-toolbar>
+    <v-content>
+      <Carousel/>
+    </v-content>
+    <Footer/>
+  </v-app>
+</template>
+
+<script>
+import Menu from "@/components/template/Menu.vue";
+import Toolbar from "@/components/template/Toolbar.vue";
+import Carousel from "@/components/widgets/Carousel.vue";
+import Footer from "@/components/template/Footer.vue";
+
+export default {
+  components: {
+    Menu,
+    Toolbar,
+    Carousel,
+    Footer
+  }
+};
+</script>
