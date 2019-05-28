9f4caa282cb3bca07769f7a2467c38c81912caf6
pilz <leandro.pizz@gmail.com>
2019-05-27 23:09:48 -0300

    CAP.140 Resumo sobre SLOTS.

---
+++ componentes/comonentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="citacao">
+    <slot name="autor"></slot>
+    <slot></slot>
+    <slot name="fonte"></slot>
+    <!-- Div especifica do Vue que aceita elementos. -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.citacao {
+  border: 1px solid #ddd;
+  background-color: rgba(0, 0, 0, 0.1);
+  padding: 25px;
+  max-width: 450px;
+}
+</style>
