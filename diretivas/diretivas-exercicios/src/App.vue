dc153a686c16ae210694c6cd98c8a82911c58d9a
pilz <leandro.pizz@gmail.com>
2019-05-31 14:39:19 -0300

    CAP.185 Registrando diretivas localmente

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -7,11 +7,35 @@
     <hr>
     <p v-destaque="cor">Usando diretiva personalizada</p>
     <p v-destaque:fundo.atrasar="cor">Usando diretiva personalizada</p>
+    <hr>
+    <p v-destaque-local="cor">Usando diretiva personalizada</p>
+    <p v-destaque-local:fundo.atrasar="cor">Usando diretiva personalizada</p>
   </div>
 </template>
 
 <script>
 export default {
+  directives: {
+    "destaque-local": {
+      bind(el, binding, vnode) {
+        // el.style.backgroundColor = 'lightgreen'
+
+        let atraso = 0;
+        if (binding.modifiers["atrasar"]) {
+          atraso = 3000;
+        }
+
+        setTimeout(() => {
+          //Valida se o argumento fundo existe e aplica a cor ao fundo.
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, atraso);
+      }
+    }
+  },
   data() {
     return {
       cor: "gray"
