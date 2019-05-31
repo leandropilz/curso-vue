874e4fc7fe2989f61363dc9170f3468473708aee
pilz <leandro.pizz@gmail.com>
2019-05-31 14:27:05 -0300

    CAP.182 Passsando Argumento para a diretiva personalizada

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -6,6 +6,7 @@
     <p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
     <hr>
     <p v-destaque="cor">Usando diretiva personalizada</p>
+    <p v-destaque:fundo="cor">Usando diretiva personalizada</p>
   </div>
 </template>
 
--- diretivas/diretivas-exercicios/src/main.js
+++ diretivas/diretivas-exercicios/src/main.js
@@ -6,7 +6,13 @@ Vue.config.productionTip = false
 Vue.directive('destaque', {
 	bind(el, binding, vnode) {
 		// el.style.backgroundColor = 'lightgreen'
-		el.style.backgroundColor = binding.value
+		
+		//Valida se o argumento fundo existe e aplica a cor ao fundo.
+		if (binding.arg === 'fundo') {
+			el.style.backgroundColor = binding.value	
+		} else {
+			el.style.color = binding.value	
+		}
 	}
 })
 
