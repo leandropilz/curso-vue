3af3428e879276f22fadfeab193502437bd822be
pilz <leandro.pizz@gmail.com>
2019-05-31 15:15:19 -0300

    CAP.186 Usando Multiplos modificadores.

--- diretivas/diretivas-exercicios/src/App.vue
+++ diretivas/diretivas-exercicios/src/App.vue
@@ -8,8 +8,8 @@
     <p v-destaque="cor">Usando diretiva personalizada</p>
     <p v-destaque:fundo.atrasar="cor">Usando diretiva personalizada</p>
     <hr>
-    <p v-destaque-local="cor">Usando diretiva personalizada</p>
-    <p v-destaque-local:fundo.atrasar="cor">Usando diretiva personalizada</p>
+    <p v-destaque-local:fundo.atrasar.alternar="'lightblue'">Usando diretiva personalizada</p>
+    <p v-destaque-local.atrasar.alternar="cor">Usando diretiva personalizada</p>
   </div>
 </template>
 
@@ -18,19 +18,32 @@ export default {
   directives: {
     "destaque-local": {
       bind(el, binding, vnode) {
-        // el.style.backgroundColor = 'lightgreen'
+        const aplicarCor = cor => {
+          //Valida se o argumento fundo existe e aplica a cor ao fundo.
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = cor;
+          } else {
+            el.style.color = cor;
+          }
+        };
 
         let atraso = 0;
         if (binding.modifiers["atrasar"]) {
           atraso = 3000;
         }
 
+        const cor1 = binding.value;
+        const cor2 = "purple";
+        let corAtual = cor1;
+
         setTimeout(() => {
-          //Valida se o argumento fundo existe e aplica a cor ao fundo.
-          if (binding.arg === "fundo") {
-            el.style.backgroundColor = binding.value;
+          if (binding.modifiers["alternar"]) {
+            setInterval(() => {
+              corAtual = corAtual === cor1 ? cor2 : cor1;
+              aplicarCor(corAtual);
+            }, 1000);
           } else {
-            el.style.color = binding.value;
+            aplicarCor(binding.value);
           }
         }, atraso);
       }
@@ -38,7 +51,7 @@ export default {
   },
   data() {
     return {
-      cor: "gray"
+      cor: "red"
     };
   }
 };
