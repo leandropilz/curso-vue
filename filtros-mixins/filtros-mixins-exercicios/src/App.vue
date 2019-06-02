ba2e696fb59cccac1df5da7893510d79227180ef
pilz <leandro.pizz@gmail.com>
2019-06-02 09:32:22 -0300

    CAP. 198 Criando mixings, e mixing globa.

--- filtros-mixins/filtros-mixins-exercicios/src/App.vue
+++ filtros-mixins/filtros-mixins-exercicios/src/App.vue
@@ -2,26 +2,46 @@
   <div id="app">
     <h1>Filtros & Mixins</h1>
     <hr>
-    <p>{{ cpf | cpf }}</p>
+    <p>{{ usuarioLogado }}</p>
+    <p>{{ cpf | cpf | inverter}}</p>
+    <input type="text" :value="cpf | cpf">
+    <hr>
+    <Fruta/>
+    <hr>
+    <div>
+      <ul>
+        <li v-for="fruta in frutas" :key="fruta">{{ fruta }}</li>
+      </ul>
+      <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
   </div>
 </template>
 
 <script>
+import frutasMixin from "./frutasMixin";
+import usuarioMixin from "./usuarioMixin";
+import Fruta from "./Fruta";
+
 export default {
+  components: { Fruta },
+  mixins: [frutasMixin, usuarioMixin],
   filters: {
-	  cpf(valor){
-		  const arr = valor.split('')
-		  arr.splice(3, 0, '.')
-		  arr.splice(7, 0, '.')
-		  arr.splice(11, 0, '-')
-		  return arr.join('')
-	  }
+    cpf(valor) {
+      const arr = valor.split("");
+      arr.splice(3, 0, ".");
+      arr.splice(7, 0, ".");
+      arr.splice(11, 0, "-");
+      return arr.join("");
+    }
   },
   data() {
     return {
       cpf: "00167164007"
     };
-  }
+  },
+  created(){
+        console.log('Created - App')
+    }
 };
 </script>
 
@@ -33,6 +53,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 2.5rem;
+}
+
+input {
   font-size: 2.5rem;
 }
 </style>
