36d431d982c0175376ba7337f7ef344ddb5e7c0e
pilz <leandro.pizz@gmail.com>
2019-06-02 10:10:10 -0300

    CAP. 200 Praticar mixing - desafio.

---
+++ filtros-mixins/filtros-mixins-desafio/src/fraseMixin.js
@@ -0,0 +1,13 @@
+export default {
+    computed: {
+        fraseComVirgula() {
+            return this.frase.replace(/\s/g, ",");
+        },
+        fraseComTamanhos() {
+            return this.frase
+                .split(" ")
+                .map(p => `${p} (${p.length})`)
+                .join(" ");
+        }
+    }
+}
