3d8b7efd7bb07e8b2b01d504189cce018d6d2077
pilz <leandro.pizz@gmail.com>
2019-06-06 22:10:56 -0300

    CAP. 219 Criando Animacao em javascript

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -8,10 +8,10 @@
       <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
     </transition>
 
-    <!-- Caso ocorra problema por diferentes tempos o atributo type determina quem deve respeitar.
-	     ao inves de usar o v-if pode ser usado o v-show  
-		 o aributo appear, faz com que explicitamente a transição ocorra ao carregar a página,
-    caso o o atributo exibir esteja = true incial ele faz a transição.-->
+    <!-- Caso ocorra problema por diferentes tempos o atributo type determina quem deve respeitar. -->
+    <!-- ao inves de usar o v-if pode ser usado o v-show -->
+    <!-- o aributo appear, faz com que explicitamente a transição ocorra ao carregar a página, -->
+    <!-- caso o o atributo exibir esteja = true incial ele faz a transição.-->
     <transition name="slide" type="animation" appear>
       <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
     </transition>
@@ -27,10 +27,28 @@
       <option value="slide">Slide</option>
     </b-select>
 
-	<!-- mode="out-in" faz com que primeiro um elemento seja feito e pós o outro. -->
+    <!-- mode="out-in" faz com que primeiro um elemento seja feito e pós o outro. -->
     <transition :name="tipoAnimacao" mode="out-in">
       <b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
       <b-alert variant="warning" show v-else key="warn">{{ msg }}</b-alert>
+    </transition>
+
+    <hr>
+    <button @click="exibir2 = !exibir2">Alternar</button>
+
+    <!-- Para desativar o CSS de forma explicita utilizar :css="false" para o caso de CSS com JS-->
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled"
+    >
+      <div v-if="exibir2" class="caixa"></div>
     </transition>
   </div>
 </template>
@@ -41,8 +59,51 @@ export default {
     return {
       msg: "Uma mensagem de informação para o usuário!",
       exibir: false,
-      tipoAnimacao: "fade"
+      exibir2: true,
+      tipoAnimacao: "fade",
+      larguraBase: 0
     };
+  },
+  methods: {
+    animar(el, done, negativo) {
+      let rodada = 1;
+      const temporizador = setInterval(() => {
+        const novaLargura =
+          this.larguraBase + (negativo ? -rodada * 10 : rodada * 10);
+        el.style.width = `${novaLargura}px`;
+        rodada++;
+        if (rodada > 30) {
+          clearInterval(temporizador);
+          done();
+        }
+      }, 20);
+    },
+    beforeEnter(el) {
+      this.larguraBase = 0;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    enter(el, done) {
+      this.animar(el, done, false);
+    },
+    // afterEnter(el) {
+    //   console.log("afterEnter");
+    // },
+    // enterCancelled(el) {
+    //   console.log("enterCancelled");
+    // },
+    beforeLeave(el) {
+      this.larguraBase = 300;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    leave(el, done) {
+      this.animar(el, done, true);
+    },
+    // afterLeave(el) {
+    //   console.log("afterLeave");
+    // },
+    // leaveCancelled(el) {
+    //   console.log("leaveCancelled");
+    // }
   }
 };
 </script>
@@ -56,6 +117,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 1.5rem;
+}
+
+.caixa {
+  height: 100px;
+  width: 300px;
+  margin: 30px auto;
+  background-color: lightgreen;
 }
 
 /* Se não configurar nome o padrão sempre será *-enter, *-enter-active */
