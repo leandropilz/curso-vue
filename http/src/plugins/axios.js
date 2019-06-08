8386847d7ee96dd86afcbfb92e038f9be180a4c8
pilz <leandro.pizz@gmail.com>
2019-06-08 16:12:06 -0300

    CAP.239 Interceptor de Resposta

--- http/src/plugins/axios.js
+++ http/src/plugins/axios.js
@@ -11,8 +11,24 @@ Vue.use({
         Vue.prototype.$http.interceptors.request.use(config => {
             // console.log(config.method)
             return config
-        })
+        }, error => Promise.reject(error))
 
+        //Utilizado para interceptar as respostas.
+        Vue.prototype.$http.interceptors.response.use(res => {
+            // Exemplo de como interceptar uma resposta e manipular os dados.
+            
+            // const array = []
+            // for (let chave in res.data) {
+            //     array.push({
+            //         id: chave,
+            //         ...res.data[chave]
+
+            //     })
+            // }
+            // res.data = array
+            return res
+
+        }, error => Promise.reject(error))
 
         // Pode-se criar instancias do axios conforme o exemplo abaixo
         // Vue.prototype.$http = axios.create({
