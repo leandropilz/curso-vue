3069476eff0767fcb9b39a1d446690b126a02680
pilz <leandro.pizz@gmail.com>
2019-06-09 11:48:29 -0300

    CAP.250 Onde estou? Estilizando o link ativo de nagegaçao do menu

--- rotas/rotas-exercicios/src/components/template/Menu.vue
+++ rotas/rotas-exercicios/src/components/template/Menu.vue
@@ -1,7 +1,14 @@
 <template>
   <nav class="menu">
-    <router-link to="/">Início</router-link>
-    <router-link to="/usuario">Usuário</router-link>
+    <ul>
+      <!-- tag, exemplo como sendo uma li, active-class indica o css para ativo exact indica o to exato -->
+      <router-link to="/" tag="li" active-class="active" exact>
+        <a>Início</a>
+      </router-link>
+      <router-link to="/usuario" tag="li" active-class="active">
+        <a>Usuário</a>
+      </router-link>
+    </ul>
   </nav>
 </template>
 
