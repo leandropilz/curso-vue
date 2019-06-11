9c35ca2b5872b087f3c0684a2fd19dac419b3eba
pilz <leandro.pizz@gmail.com>
2019-06-11 18:09:00 -0300

    CAP. 303 Adicionando o botao comprar.

--- projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
+++ projeto-03-acoes/stock-trader/src/components/stocks/Stock.vue
@@ -3,22 +3,44 @@
     <v-card class="green darken-3 white--text">
       <v-card-title class="headline">
         <strong>
-          Nome
-          <small>(Preço: PRECO)</small>
+          {{ stock.name }}
+          <small>(Preço: {{ stock.price }})</small>
         </strong>
       </v-card-title>
     </v-card>
     <v-card>
       <v-container fill-height>
-        <v-text-field label="Quantidade" type="number"/>
-        <v-btn class="green darken-3 white--text">Comprar</v-btn>
+        <v-text-field label="Quantidade" type="number" v-model.number="quantity"/>
+        <v-btn
+          @click="buyStock"
+          class="green darken-3 white--text"
+          :disabled="quantity <= 0 || !Number.isInteger(quantity) "
+        >Comprar</v-btn>
       </v-container>
     </v-card>
   </v-flex>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["stock"],
+  data() {
+    return {
+      quantity: 0
+    };
+  },
+  methods: {
+    buyStock() {
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+      };
+
+      this.quantity = 0;
+    }
+  }
+};
 </script>
 
 <style>
